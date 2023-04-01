ddaafeb27ed3f5e2393ca598217a6ba4e774655f
chenchunyang <chenchunyang@kuaishou.com>
2023-04-01 13:33:06 +0800

    feat: coffees

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get('sweet')
+  findSweet() {
+    return 'A sweet coffee';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+}
